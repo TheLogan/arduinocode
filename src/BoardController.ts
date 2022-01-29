802aef00c05956e2d8d7e0d94b60fbb91659c11a
TheLogan <timlogan475@gmail.com>
2022-01-29 15:09:54 +0100

    changed import

--- src/BoardController.ts
+++ src/BoardController.ts
@@ -1,8 +1,9 @@
-import { Board, Servo } from "johnny-five";
+// import { Board, Servo } from "johnny-five";
 // import { RaspiIO} from "raspi-io";
+import five from "johnny-five";
 
 let Raspi = require("raspi-io").RaspiIO;
-let board = new Board({
+let board = new five.Board({
   io: new Raspi()
 });
 
@@ -10,12 +11,12 @@ board.on("ready", function () {
   const controller = "PCA9685" ;
   // let led = new five.Led("P1-13");
 
-  const a = new Servo({
+  const a = new five.Servo({
     controller,
     pin: 0,
   });
 
-  const b = new Servo({
+  const b = new five.Servo({
     controller,
     range: [0, 180],
     pin: 1,
